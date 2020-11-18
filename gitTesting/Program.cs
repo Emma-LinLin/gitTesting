1bfa4d83f09dafb19b99ff6542545fe00173655e
emmalinnearoos@gmail.com <emmalinnearoos@gmail.com>
2020-11-18 20:47:21 +0100

    Initial commit

---
+++ gitTesting/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace gitTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
