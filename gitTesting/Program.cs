77af36eb68de770fa89c02452ea9a8cbbf17b7c1
emmalinnearoos@gmail.com <emmalinnearoos@gmail.com>
2020-11-18 21:16:21 +0100

    added listname through user input, looped array

--- gitTesting/Program.cs
+++ gitTesting/Program.cs
@@ -7,6 +7,9 @@ namespace gitTesting
         static void Main(string[] args)
         {
             Console.WriteLine("Hi! You will now be able to create your own top-list");
+            Console.WriteLine("What would you like to name your toplist?");
+            string listName = Console.ReadLine();
+
             Console.WriteLine("How many places would you like your list to contain?: ");
 
             int topPlaces; 
@@ -16,7 +19,13 @@ namespace gitTesting
                 Console.WriteLine("Please try again, only intergers.");
             }
 
-            int[] userList = new int[topPlaces];
+            string[] userList = new string [topPlaces];
+
+            for(int i = 0; i < userList.Length; i++)
+            {
+                Console.WriteLine($"Enter position {i + 1}: ");
+                userList[i] = Console.ReadLine();
+            }
         }
     }
 }
