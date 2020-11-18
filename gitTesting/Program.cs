83fbed7d9a49fe5e040cc8b7be03caf70b93f50f
emmalinnearoos@gmail.com <emmalinnearoos@gmail.com>
2020-11-18 21:01:54 +0100

    created array based on userinput

--- gitTesting/Program.cs
+++ gitTesting/Program.cs
@@ -6,7 +6,17 @@ namespace gitTesting
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hi! You will now be able to create your own top-list");
+            Console.WriteLine("How many places would you like your list to contain?: ");
+
+            int topPlaces; 
+            
+            while(int.TryParse(Console.ReadLine(), out topPlaces) == false)
+            {
+                Console.WriteLine("Please try again, only intergers.");
+            }
+
+            int[] userList = new int[topPlaces];
         }
     }
 }
